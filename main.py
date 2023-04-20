c48469085fefe51863e07f04864acd4b97b67f74
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2023-04-20 21:19:52 +0400

    add main function

--- main.py
+++ main.py
@@ -9,30 +9,35 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-  keyboard = telebot.types.ReplyKeyboardMarkup()
-  red_button = telebot.types.KeyboardButton("🟥")
-  black_button = telebot.types.KeyboardButton("⬛️")
-  keyboard.row(red_button)
-  keyboard.row(black_button)
-  bot.send_message(message.chat.id, "What is color: 🟥 or ⬛️", reply_markup=keyboard)
-  bot.register_next_step_handler(message, answer_card)
+	keyboard = telebot.types.ReplyKeyboardMarkup()
+	red_button = telebot.types.KeyboardButton("🟥")
+	black_button = telebot.types.KeyboardButton("⬛️")
+	keyboard.row(red_button)
+	keyboard.row(black_button)
+	bot.send_message(message.chat.id, "What is color: 🟥 or ⬛️", reply_markup=keyboard)
+	bot.register_next_step_handler(message, answer_card)
 
 
 def answer_card(message):
-  value, suit = random_card()
-  if message.text == "🟥" and suit in ["H", "D"]:
-    bot.send_message(message.chat.id, f"Correct. Card was {value}{suit}")
-  elif message.text == "⬛️" and suit in ["C", "S"]: #else + if = elif:
-    bot.send_message(message.chat.id, f"Correct. Card was {value}{suit}")
-  else:
-    bot.send_message(message.chat.id, f"Incorrect. Card was {value}{suit}")
-  start(message)
+	value, suit = random_card()
+	if message.text == "🟥" and suit in ["H", "D"]:
+		bot.send_message(message.chat.id, f"Correct. Card was {value}{suit}")
+	elif message.text == "⬛️" and suit in ["C", "S"]: #else + if = elif:
+		bot.send_message(message.chat.id, f"Correct. Card was {value}{suit}")
+	else:
+		bot.send_message(message.chat.id, f"Incorrect. Card was {value}{suit}")
+	start(message)
 
 
 def random_card():
-  value = random.choice(["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"])
-  suit = random.choice(["H", "D", "C", "S"])
-  return value, suit
+	value = random.choice(["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"])
+	suit = random.choice(["H", "D", "C", "S"])
+	return value, suit
 
 
-bot.infinity_polling()
+def main():
+	bot.infinity_polling()
+
+
+if __name__ == '__main__':
+	main()
